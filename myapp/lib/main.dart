efa7977f7835c727e6a2c03742412c2bbb6a766b
hildemberg986 <hildembergeling@gmail.com>
2024-09-25 15:51:21 -0300

    Creat project

---
+++ myapp/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main() {
+  MaterialApp app = MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(
+            title: const Text(
+          "Cachaça Hub",
+           style: GoogleFonts.getFont('Lato'),
+        )),
+        body: const Center(
+            child: Column(children: [
+          Text("Apenas começando..."),
+          Text("No meio..."),
+          Text("Terminando...")
+        ])),
+        bottomNavigationBar: const Text("Botão 1"),
+      ));
+
+  runApp(app);
+}
