5e1607f8f27786421092a7b91d5fefeeb99ee19a
hildemberg986 <hildembergeling@gmail.com>
2024-10-02 14:37:08 -0300

    Corrigir erro Swift no AppDelegate

--- myapp/ios/Runner/AppDelegate.swift
+++ myapp/ios/Runner/AppDelegate.swift
@@ -1,25 +1,13 @@
 import UIKit
 import Flutter
 
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    override func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    // Aqui está o código dentro de uma função
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
 }
-
-// Método main() para iniciar a aplicação
-func main() {
-    UIApplicationMain(
-        CommandLine.argc,
-        CommandLine.unsafeArgv,
-        nil, // O nome da classe do aplicativo pode ser especificado aqui se necessário.
-        NSStringFromClass(AppDelegate.self) // Classe AppDelegate
-    )
-}
-
-// Chame o método main() para iniciar a aplicação
-main()
