fffda7b2890f6721c89d34fcf2535dddaef706de
hildemberg986 <hildembergeling@gmail.com>
2024-10-02 13:29:35 -0300

    Creating sections to access the pages loja, feedback e avaliações, notificação, patrocinadores, perfil and saúde e bem estar

--- myapp/lib/widgets/custom_drawer.dart
+++ myapp/lib/widgets/custom_drawer.dart
@@ -2,6 +2,12 @@ import 'package:flutter/material.dart';
 import '../screens/noticias_screen.dart';
 import '../screens/eventos_screen.dart';
 import '../screens/jogos_screen.dart';
+import '../screens/patrocinadores_screen.dart';
+import '../screens/feedback_e_avaliacoes_screen.dart';
+import '../screens/loja_screen.dart';
+import '../screens/notificacao_screen.dart';
+import '../screens/perfil_screen.dart';
+import '../screens/saude_e_bem_estar_screen.dart';
 
 class CustomDrawer extends StatelessWidget {
   final Function(Widget) onSelectScreen;
@@ -23,10 +29,28 @@ class CustomDrawer extends StatelessWidget {
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 35,
-                fontFamily: 'Raleway', 
+                fontFamily: 'Raleway',
                 fontWeight: FontWeight.bold,
               ),
             )),
+          ),
+          ListTile(
+            leading: const Icon(Icons.person_pin_rounded),
+            title: const Text('Perfil'),
+            onTap: () {
+              Navigator.pop(context); // Fecha o drawer
+              onSelectScreen(
+                  const PerfilScreen()); // Atualiza o conteúdo da tela
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications_active_outlined),
+            title: const Text('Notificações'),
+            onTap: () {
+              Navigator.pop(context); // Fecha o drawer
+              onSelectScreen(
+                  const NotificacaoScreen()); // Atualiza o conteúdo da tela
+            },
           ),
           ListTile(
             leading: const Icon(Icons.newspaper),
@@ -53,6 +77,41 @@ class CustomDrawer extends StatelessWidget {
               Navigator.pop(context); // Fecha o drawer
               onSelectScreen(
                   const JogosScreen()); // Atualiza o conteúdo da tela
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.shopping_bag_outlined),
+            title: const Text('Loja'),
+            onTap: () {
+              Navigator.pop(context); // Fecha o drawer
+              onSelectScreen(const LojaScreen()); // Atualiza o conteúdo da tela
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.monitor_heart_outlined),
+            title: const Text('Saúde e Bem-Estar'),
+            onTap: () {
+              Navigator.pop(context); // Fecha o drawer
+              onSelectScreen(
+                  const SaudeEBemEstarScreen()); // Atualiza o conteúdo da tela
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.handshake_outlined),
+            title: const Text('Patrocinadores'),
+            onTap: () {
+              Navigator.pop(context); // Fecha o drawer
+              onSelectScreen(
+                  const PatrocinadoresScreen()); // Atualiza o conteúdo da tela
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.feedback_outlined),
+            title: const Text('Feedback e Avaliações'),
+            onTap: () {
+              Navigator.pop(context); // Fecha o drawer
+              onSelectScreen(
+                  const FeedbackEAvaliacoesScreen()); // Atualiza o conteúdo da tela
             },
           ),
         ],
